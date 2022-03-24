25e7a0c7e82a5377049bbe1c8184963e5be59ca7
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2022-03-24 19:13:29 -0300

    módulo 2 finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
-export default function Home() {
+import { Flex, Stack, Button} from "@chakra-ui/react";
+import { Input } from "../components/Form/input"
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
-  )
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360} // 360px
+        bg="gray.800"
+        p="8" // 2rem ou 32px
+        borderRadius={8} // 8px
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg" >Entrar</Button>
+      </Flex>
+    </Flex>
+  );
 }
